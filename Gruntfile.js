bd553061ded4ad64570d74df8e8d2f65b3941dc3
Fahad <iqbal.fahad90@gmail.com>
2014-12-31 09:09:14 -0600

    Initail Commit

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(grunt) {
+
+grunt.initConfig({});
+
+# Load grunt modules
+  grunt.loadNpmTasks('grunt-contrib-concat')
+  grunt.loadNpmTasks('grunt-contrib-copy')
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-uglify')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+
+};
